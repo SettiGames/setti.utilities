bc3e1fdf3e529af93f3432f897e772e2abd389ef
🍕 Chase McClure <chase@studioofthemonth.com>
2018-01-11 13:32:09 -0600

    Adds socket errors

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -46,6 +46,7 @@ export namespace error {
     }
 
     const enum Server {
+        missingSecret = 'missingSocketSecret',
         invalidSecret = 'invalidSocketSecret',
         noServers = 'noSocketServers',
         hubActionFailed = 'hubActionFailed'
